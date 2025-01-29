96d9c6b85a7cc0b8abc4e68289bf93ea192c398a
SergioBadia <sergarba16@gmail.com>
2025-01-29 12:57:24 +0100

    He añadido una suma
    Signed-off-by: SergioBadia <sergarba16@gmail.com>

---
+++ pruebarepositorio/src/pruebarepositorio/Prueba.java
@@ -0,0 +1,14 @@
+package pruebarepositorio;
+
+public class Prueba {
+
+	public static void main(String[] args) {
+		System.out.println("Hola mundo");
+
+		
+		int a = 3;
+		int b = 8;
+		System.out.println(a+b); //Suma
+	}
+
+}
